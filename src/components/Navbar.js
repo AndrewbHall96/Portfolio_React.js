5e656200148b031bff893b2f8724ea7cb563f0de
Andrew Hall <andrewbhall96@gmail.com>
2020-08-08 17:25:21 -0400

    addded cards, footer and navbar files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,23 @@
 import React from "react";
 // import "../styles/Footer.css";
+import Navbar from 'react-bootstrap/Navbar';
 import Navbar from 'react-bootstrap/Navbar'
 
-function Navbar ()  {
+function Navbar() {
     return (
         <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+
+
     );
 }
